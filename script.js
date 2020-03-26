7e43af9a20b26559e1c64cfdae755f588284123f
gbolahan146 <gbolahanoduyemi1@gmail.com>
2020-03-26 02:51:20 +0100

    Js task

---
+++ script.js
@@ -0,0 +1,20 @@
+// Nothing here yet
+
+let name = "Gbolahan Oduyemi";
+const courses = ["Mobile", "Frontend", "Coding"];
+
+console.log(name);
+console.log(courses);
+
+num = [];
+for (let i=0; i<=200; i++) {
+
+    if (i%2==!0) {
+ 
+      num.push(i);
+ 
+    }
+ 
+  }
+ 
+  console.log(num);
